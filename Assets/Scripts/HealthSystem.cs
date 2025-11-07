31731bc6e3ff5e0a39b61148a6719662f2592f16
Serhii Kozlov <sk0621o@gre.ac.uk>
2025-11-07 15:58:59 +0000

    Add win condition, fix collision, replace character controller

---
+++ Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,46 @@
+using System;
+using UnityEngine;
+
+public class HealthSystem : MonoBehaviour
+{
+    [SerializeField] private int health;
+    [SerializeField] private int maxHealth = 5;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        health = maxHealth;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage()
+    {
+        Debug.Log("Take damage");
+        health--;
+        Mathf.Clamp(health, 0f, 5f); // For some reason it doesn't clamp the value, moving forward...
+
+        if (health <= 0)
+        {
+            PlayerController playerController = GetComponentInParent<PlayerController>();
+            string playerName = playerController.GetPlayerName();
+            Manager.Instance.EndGame(playerName);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("OnCollisionEnter");
+        if (other.gameObject == null)
+            return;
+        
+        Debug.Log($"Collided gameobject name: {other.gameObject.name}");
+        if (other.gameObject.CompareTag("Hit") )
+        {
+            TakeDamage();
+        }
+    }
+}
